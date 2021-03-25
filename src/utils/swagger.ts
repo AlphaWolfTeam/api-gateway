d5fd36bbe5acd9096bf4922af633ae65e472e413
CarmelTsadok <carmelcarmel88888@gmail.com>
2021-03-25 14:08:45 +0200

    feat: Add user header and roles sections

--- src/utils/swagger.ts
+++ src/utils/swagger.ts
@@ -10,7 +10,12 @@ const swaggerDocument = {
       name: 'Groups',
     },
     {
+      name: 'User Header',
+      description: 'Most of the API endpoints require a `requesterID` - the user ID of the requester. The ID should be sent in a header (usually `X-User-ID`). Endpoints that do not always require a requester may return a Forbidden error in some cases where `requesterID` is necessary. Therefore sending a `requesterID` is always advised.',
+    },
+    {
       name: 'Roles and Permissions',
+      description: "The endpoints that require a `requesterID` validate that the user has permission for the action he requested. The service finds the requester role in the group and then compares it with the required role for that action by the `requiredRole` found in this file. The requester must be in a group in order to make CRUD requests on it unless it's a GET on a public group.",
     },
   ],
   paths: {
@@ -26,7 +31,7 @@ const swaggerDocument = {
             name: 'X-User-ID',
             type: 'string',
             format: 'uuid',
-            description: '`requesterID` - the user ID of the requester. Required sometimes.',
+            description: "`requesterID` - the user ID of the requester. \nRequired sometimes.\nThe requester must be in a group in order to make request on it unless it's a public group.",
             required: true,
           },
           {
@@ -50,7 +55,7 @@ const swaggerDocument = {
             schema: {
               type: 'array',
               items: {
-                $ref: '#/definitions/Response',
+                $ref: '#/definitions/Group-Primal',
               },
             },
           },
@@ -83,7 +88,7 @@ const swaggerDocument = {
             in: 'body',
             name: 'body',
             schema: {
-              $ref: '#/definitions/Group',
+              $ref: '#/definitions/Create-Group',
             },
             description: '',
             required: true,
@@ -95,7 +100,144 @@ const swaggerDocument = {
             schema: {
               type: 'array',
               items: {
-                $ref: '#/definitions/Response',
+                $ref: '#/definitions/Group-Primal',
+              },
+            },
+          },
+          400: {
+            description: 'Validation error',
+          },
+          401: {
+            description: 'Unauthorized',
+          },
+          500: {
+            description: 'Server side error',
+          },
+        },
+      },
+    },
+    '/groups/{id}': {
+      get: {
+        tags: [
+          'Groups',
+        ],
+        parameters: [
+          {
+            in: 'header',
+            name: 'X-User-ID',
+            type: 'string',
+            format: 'uuid',
+            description: "`requesterID` - the user ID of the requester. \nRequired sometimes.\nThe requester must be in a group in order to make request on it unless it's a public group.",
+            required: true,
+          },
+          {
+            in: 'path',
+            name: 'id',
+            type: 'string',
+            description: 'The id of the group',
+            required: true,
+          },
+        ],
+        summary: 'Get group by ID',
+        responses: {
+          200: {
+            description: 'OK',
+            schema: {
+              $ref: '#/definitions/Group-Primal',
+            },
+          },
+          400: {
+            description: 'Validation error',
+          },
+          401: {
+            description: 'Unauthorized',
+          },
+          500: {
+            description: 'Server side error',
+          },
+        },
+      },
+      patch: {
+        tags: [
+          'Groups',
+        ],
+        summary: 'Update Group',
+        parameters: [
+          {
+            in: 'header',
+            name: 'X-User-ID',
+            type: 'string',
+            format: 'uuid',
+            description: '`requesterID` - the user ID of the requester.',
+            required: true,
+          },
+          {
+            in: 'path',
+            name: 'id',
+            type: 'string',
+            description: 'The id of the group',
+            required: true,
+          },
+          {
+            in: 'body',
+            name: 'body',
+            schema: {
+              $ref: '#/definitions/Update-Group',
+            },
+            description: '',
+            required: true,
+          },
+        ],
+        responses: {
+          200: {
+            description: 'OK',
+            schema: {
+              $ref: '#/definitions/Group-Primal',
+            },
+          },
+          400: {
+            description: 'Validation error',
+          },
+          401: {
+            description: 'Unauthorized',
+          },
+          500: {
+            description: 'Server side error',
+          },
+        },
+      },
+      delete: {
+        tags: [
+          'Groups',
+        ],
+        summary: 'Delete group',
+        parameters: [
+          {
+            in: 'header',
+            name: 'X-User-ID',
+            type: 'string',
+            format: 'uuid',
+            description: '`requesterID` - the user ID of the requester.',
+            required: true,
+          },
+          {
+            in: 'path',
+            name: 'id',
+            type: 'string',
+            description: 'The id of the group',
+            required: true,
+          },
+        ],
+        responses: {
+          200: {
+            description: 'OK',
+            schema: {
+              type: 'object',
+              properties: {
+                _id: {
+                  type: 'string',
+                  format: 'ObjectId',
+                },
               },
             },
           },
@@ -113,7 +255,7 @@ const swaggerDocument = {
     },
   },
   definitions: {
-    Group: {
+    'Create-Group': {
       type: 'object',
       properties: {
         name: {
@@ -156,6 +298,24 @@ const swaggerDocument = {
         },
       },
     },
+    'Update-Group': {
+      type: 'object',
+      properties: {
+        name: {
+          type: 'string',
+        },
+        description: {
+          type: 'string',
+        },
+        icon: {
+          type: 'string',
+          format: 'base64',
+        },
+        type: {
+          $ref: '#/definitions/Group-Type',
+        },
+      },
+    },
     'Group-Type': {
       type: 'string',
       enum: [
@@ -171,7 +331,7 @@ const swaggerDocument = {
         'Admin',
       ],
     },
-    Response: {
+    'Group-Primal': {
       type: 'object',
       properties: {
         _id: {
